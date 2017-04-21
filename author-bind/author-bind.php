34889eed61c623be672b4df196bb87beeec2a79c
Jarl André Hübenthal <jarl.andre@gmail.com>
2017-04-21 15:09:26 +0200

    Updating header

--- author-bind/author-bind.php
+++ author-bind/author-bind.php
@@ -1,7 +1,29 @@
 <?php
-/*
-Plugin Name: Author Bind
-*/
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://github.com/jarlah/Author-Bind
+ * @since             1.0.0
+ * @package           Author_Bind
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Author Bind
+ * Plugin URI:        http://github.com/jarlah/Author-Bind
+ * Description:       Author Bind is a plugin that lets you configure a User ID to override all future added or edited posts/pages.
+ * Version:           1.0.0
+ * Author:            Jarl André Hübenthal
+ * Author URI:        http://github.com/jarlah/Author-Bind
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       author-bind
+ * Domain Path:       /languages
+ */
 
 defined('ABSPATH') or die('No script kiddies please!');
 
